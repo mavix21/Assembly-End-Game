f0823a02170f79f645abacf3bc87321bc117958a
Marcelo <marcelo.vizcarra@inventiva-solutions.com>
2024-12-21 23:55:58 -0500

    Feat: display the word when the game is lost

--- src/components/Word.tsx
+++ src/components/Word.tsx
@@ -1,17 +1,28 @@
+import clsx from "clsx";
+
 interface WordProps {
   word: string;
   guessedLetters: string[];
+  isGameLost: boolean;
 }
 
-export function Word({ word, guessedLetters }: WordProps) {
+export function Word({ word, guessedLetters, isGameLost }: WordProps) {
   return (
     <div className="flex gap-1">
       {word.split("").map((letter, index) => (
         <span
           key={index}
-          className="text-2xl w-12 aspect-square bg-muted border-b border-b-border p-2 text-white grid place-items-center"
+          className={clsx(
+            "text-2xl w-12 aspect-square bg-muted border-b border-b-border p-2 text-white grid place-items-center",
+            {
+              "!text-red-400": isGameLost && !guessedLetters.includes(letter),
+            },
+          )}
         >
-          {guessedLetters.includes(letter) ? letter.toUpperCase() : ""}
+          {isGameLost ||
+              guessedLetters.includes(letter)
+            ? letter.toUpperCase()
+            : ""}
         </span>
       ))}
     </div>
